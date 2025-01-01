1de980d49372ee9516ab0246d26afb77dec6868c
Glila Bousselham <glilabousselham01@gmail.com>
2025-01-01 19:06:21 +0100

    complate the cd command

--- src/commands/ChangeDirCommand.java
+++ src/commands/ChangeDirCommand.java
@@ -3,8 +3,12 @@ package commands;
 import command.AbstractCommand;
 import command.WorkingDir;
 
+import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ChangeDirCommand extends AbstractCommand {
-    public ChangeDirCommand(WorkingDir file){
+    public ChangeDirCommand(WorkingDir file) {
         super(file);
     }
 
@@ -12,22 +16,34 @@ public class ChangeDirCommand extends AbstractCommand {
     @Override
     public void execute(String command) {
 
+        // trim the command
+        String path = command.trim();
+
         // check the path that we want to navigate into
-        if (command.compareTo("..") == 0){
+        if (path.compareTo("..") == 0) {
             this.currentDirectory.setWorkingDir(this.currentDirectory.getWorkingDir().getParentFile());
-        }else {
-            // must be like dir/dir2... or dir
-
-
+            return;
         }
 
 
+        // must be like dir/dir2... or dir
+        Pattern pattern = Pattern.compile("^[a-z][a-z0-9]{0,}$", Pattern.CASE_INSENSITIVE);
+        if (pattern.matcher(path).find()) {
 
-        System.out.println(command);
+            File newFile = new File(this.currentDirectory.getWorkingDir(), path);
+
+            // check if directory
+            if(!newFile.isDirectory()) {
+                System.err.println(path + " is not a directory");
+                return;
+            }
+
+            this.currentDirectory.setWorkingDir(newFile);
+        }
     }
 
 
-    public static String getCommandName(){
+    public static String getCommandName() {
         return "cd";
     }
 }
